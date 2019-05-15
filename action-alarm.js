d2a648c09fb0cbe7c187e5df84bc980209e0f5c3
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-05-15 17:20:05 +0200

    Updated dependencies + disabled debug printing

--- action-alarm.js
+++ action-alarm.js
@@ -7,8 +7,8 @@ const debug = require('debug')
 const { name } = require('./package.json')
 
 // Enable error print
-//debug.enable(name + ':error')
+debug.enable(name + ':error')
 // Uncomment this line to print everything
-debug.enable(name + ':*')
+//debug.enable(name + ':*')
 
 require('./dist/index').default()
